4e84525d5707a0ba3254cf1b86ff40764a66fb0b
Caio Lucena <caio.hlucena@hotmail.com>
2023-04-06 09:58:14 -0300

    apps

---
+++ lecture3/hello/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "hello/index.html")
+
+def caio(request):
+    return HttpResponse("Hello, Caio")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        'name': name.capitalize()
+        })
